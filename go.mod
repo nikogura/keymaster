85ad47e669db0736a627422da3895300bd8f9e91
Nik Ogura <nik@scribd.com>
2019-07-06 08:55:35 -0700

    Converted to use vaultttest pkg

--- go.mod
+++ go.mod
@@ -3,13 +3,16 @@ module git.lo/dbt/psst
 go 1.12
 
 require (
-	git.lo/ops/scrutil v0.0.0-20190625152842-1fad7ffebef4
-	github.com/davecgh/go-spew v1.1.1
+	git.lo/ops/vaulttest v0.0.0-20190706155433-e0d4b5bb9ff5
 	github.com/google/uuid v1.1.1
-	github.com/hashicorp/vault/api v1.0.2
+	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/phayes/freeport v0.0.0-20180830031419-95f893ade6f2
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.4.0
 	github.com/stretchr/testify v1.3.0
+	golang.org/x/sys v0.0.0-20190520201301-c432e742b0af // indirect
+	golang.org/x/text v0.3.2 // indirect
 )
--- pkg/psst/psst_test.go
+++ pkg/psst/psst_test.go
@@ -1,27 +1,17 @@
 package psst
 
 import (
-	"bufio"
 	"fmt"
-	"github.com/hashicorp/vault/api"
-	"github.com/mitchellh/go-homedir"
+	"git.lo/ops/vaulttest/pkg/vaulttest"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"regexp"
-	"strings"
 	"testing"
 )
 
 var tmpDir string
-var testServer *exec.Cmd
-var testServerRunning bool
-var testUnsealKey string
-var testRootToken string
-var userToken string
-var userTokenFile string
+var testServer *vaulttest.VaultDevServer
 
 func TestMain(m *testing.M) {
 	setUp()
@@ -42,122 +32,27 @@ func setUp() {
 
 	tmpDir = dir
 
-	// find the user's vault token file if it exists
-	homeDir, err := homedir.Dir()
-	if err != nil {
-		log.Fatalf("Unable to determine user's home dir: %s", err)
+	testServer = vaulttest.NewVaultDevServer("")
+
+	if !testServer.Running {
+		testServer.ServerStart()
 	}
 
-	userTokenFile = fmt.Sprintf("%s/.vault-token", homeDir)
-
-	// read it into memory cos the test server is gonna overwrite it
-	if _, err := os.Stat(userTokenFile); !os.IsNotExist(err) {
-		tokenBytes, err := ioutil.ReadFile(userTokenFile)
-		if err == nil {
-			userToken = string(tokenBytes)
-		}
-	}
-
-	// only start if we're not already running (all tests will run this setUp function
-	if !testServerRunning {
-		vault, err := exec.LookPath("vault")
-		if err != nil {
-			log.Fatal("'vault' is not installed and available on the path")
-		}
-
-		testServer = exec.Command(vault, "server", "-dev")
-
-		testServer.Stderr = os.Stderr
-		out, err := testServer.StdoutPipe()
-		if err != nil {
-			log.Fatalf("unable to connect to testserver's stdout: %s", err)
-		}
-
-		err = testServer.Start()
-
-		scanner := bufio.NewScanner(out)
-
-		unsealPattern := regexp.MustCompile(`^Unseal Key:.+`)
-		rootTokenPattern := regexp.MustCompile(`^Root Token:.+`)
-
-		for testUnsealKey == "" || testRootToken == "" {
-			scanner.Scan()
-			line := scanner.Text()
-
-			if testUnsealKey == "" && unsealPattern.MatchString(line) {
-				parts := strings.Split(line, ": ")
-				if len(parts) > 1 {
-					testUnsealKey = parts[1]
-					strings.TrimRight(testUnsealKey, "\n")
-					strings.TrimLeft(testUnsealKey, " ")
-				}
-
-				continue
-			}
-
-			if testRootToken == "" && rootTokenPattern.MatchString(line) {
-				parts := strings.Split(line, ": ")
-				if len(parts) > 1 {
-					testRootToken = parts[1]
-					strings.TrimRight(testUnsealKey, "\n")
-					strings.TrimLeft(testUnsealKey, " ")
-				}
-
-				continue
-			}
-
-		}
-
-		// wait a bit for things to be ready.
-		//fmt.Printf("Waiting 3 seconds for the test server to spool up.\n")
-		//time.Sleep(3 * time.Second)
-
-		testServerRunning = true
-
-	}
 }
 
 func tearDown() {
-	if testServerRunning {
-		testServer.Process.Kill()
-	}
-
 	if _, err := os.Stat(tmpDir); !os.IsNotExist(err) {
 		os.Remove(tmpDir)
 	}
 
-	// restore the user's vault token when we're done.
-	if userToken != "" {
-		_ = ioutil.WriteFile(userTokenFile, []byte(userToken), 0600)
-	}
-}
-
-// VaultTestClient returns a configured vault client for the test vault server.  By default the client returned has the root token for the test vault instance set.  If you want something else, you will need to reconfigure it.
-func VaultTestClient() *api.Client {
-	config := api.DefaultConfig()
-
-	err := config.ReadEnvironment()
-	if err != nil {
-		log.Fatalf("failed to inject environment into test vault client config")
-	}
-
-	config.Address = "http://127.0.0.1:8200"
-
-	client, err := api.NewClient(config)
-	if err != nil {
-		log.Fatalf("failed to create test vault api client: %s", err)
-	}
-
-	client.SetToken(testRootToken)
-
-	return client
+	testServer.ServerShutDown()
 
 }
 
 func TestVaultTestClient(t *testing.T) {
 	assert.True(t, 1 == 1, "the law of identity has been broken")
 
-	client := VaultTestClient()
+	client := testServer.VaultTestClient()
 
 	secret, err := client.Logical().Read("secret/config")
 	if err != nil {
