57d4fa4596558ee6d8c5b6b94cee00c2068de547
Nik Ogura <nik@scribd.com>
2019-07-17 11:35:36 -0700

    Changed name to 'harpo'

---
+++ pkg/harpo/harpo.go
@@ -0,0 +1,86 @@
+package harpo
+
+// Environment Scribd's deployment environments.   One of "Prod", "Stage", "Dev"
+type Environment int
+
+const (
+	Prod Environment = iota + 1
+	Stage
+	Dev
+)
+
+// Generator an interface for a function that creates a string according to a pattern.  E. g.
+type Generator interface {
+	Generate() string
+}
+
+// How do we maintain Environment distinctions?  One easy way is to have 3 separate Vault instances.
+// How can we do with 1?  Should we?
+
+type Org struct {
+	Name  string `yaml:"org""`
+	Roles []Role `yaml:"roles"`
+}
+
+type Role struct {
+	Org     string   `yaml:"org"`
+	Name    string   `yaml:"name"`
+	Secrets []Secret `yaml:"secrets"`
+}
+
+type Secret struct {
+	Name      string    `yaml:"name"`
+	Generator Generator `yaml:"generator"`
+
+	DevValue   string `yaml:"dev_value"`
+	StageValue string `yaml:"stage_value"`
+	ProdValue  string `yaml:"prod_value"`
+}
+
+type SecretMetadata struct {
+	/*
+		{
+		  "data": {
+		    "created_time": "2018-03-22T02:24:06.945319214Z",
+		    "current_version": 3,
+		    "max_versions": 0,
+		    "oldest_version": 0,
+		    "updated_time": "2018-03-22T02:36:43.986212308Z",
+		    "versions": {
+		      "1": {
+		        "created_time": "2018-03-22T02:24:06.945319214Z",
+		        "deletion_time": "",
+		        "destroyed": false
+		      },
+		      "2": {
+		        "created_time": "2018-03-22T02:36:33.954880664Z",
+		        "deletion_time": "",
+		        "destroyed": false
+		      },
+		      "3": {
+		        "created_time": "2018-03-22T02:36:43.986212308Z",
+		        "deletion_time": "",
+		        "destroyed": false
+		      }
+		    }
+		  }
+		}
+	*/
+
+}
+
+/*
+	LDAP Auth can access only dev secrets
+
+	k8s dev auth can access dev secrets
+	k8s stage auth can access stage secrets
+	k8s prod auth can access prod secrets
+
+	aws dev auth can access dev secrets
+	aws stage auth can access stage secrets
+	aws prod auth can access prod secrets
+
+	tls prod auth can access prod secrets
+	do we need tls stage / dev access at all?
+
+*/
---
+++ pkg/harpo/harpo_test.go
@@ -0,0 +1,72 @@
+package harpo
+
+import (
+	"fmt"
+	"git.lo/ops/vaulttest/pkg/vaulttest"
+	"github.com/phayes/freeport"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+)
+
+var tmpDir string
+var testServer *vaulttest.VaultDevServer
+
+func TestMain(m *testing.M) {
+	setUp()
+
+	code := m.Run()
+
+	tearDown()
+
+	os.Exit(code)
+}
+
+func setUp() {
+	dir, err := ioutil.TempDir("", "psst")
+	if err != nil {
+		fmt.Printf("Error creating temp dir %q: %s\n", tmpDir, err)
+		os.Exit(1)
+	}
+
+	tmpDir = dir
+
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		log.Fatalf("Failed to get a free port on which to run the test vault server: %s", err)
+	}
+
+	testAddress := fmt.Sprintf("127.0.0.1:%d", port)
+
+	testServer = vaulttest.NewVaultDevServer(testAddress)
+
+	if !testServer.Running {
+		testServer.ServerStart()
+	}
+
+}
+
+func tearDown() {
+	if _, err := os.Stat(tmpDir); !os.IsNotExist(err) {
+		os.Remove(tmpDir)
+	}
+
+	testServer.ServerShutDown()
+
+}
+
+func TestVaultTestClient(t *testing.T) {
+	assert.True(t, 1 == 1, "the law of identity has been broken")
+
+	client := testServer.VaultTestClient()
+
+	secret, err := client.Logical().Read("secret/config")
+	if err != nil {
+		log.Printf("Failed to default secret config: %s", err)
+		t.Fail()
+	}
+
+	assert.True(t, secret != nil, "We got a secret from the test vault server")
+}
