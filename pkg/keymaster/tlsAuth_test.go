f091392c3a83a82fc7b5e8536af711055ac9c1ad
Nik Ogura <nik@scribd.com>
2019-09-17 10:23:06 -0700

    updated test code for change in ldap

--- pkg/keymaster/tlsAuth_test.go
+++ pkg/keymaster/tlsAuth_test.go
@@ -17,14 +17,14 @@ func TestTlsAuthCrud(t *testing.T) {
 		Secrets: []*Secret{
 			{
 				Name:      "foo",
-				Namespace: "bluens",
+				Namespace: "testns1",
 				Generator: AlphaGenerator{
 					Type:   "alpha",
 					Length: 10,
 				},
 			},
 		},
-		Namespace: "bluens",
+		Namespace: "testns1",
 	}, Prod)
 	if err != nil {
 		log.Printf("error creating policy: %s", err)
@@ -37,14 +37,14 @@ func TestTlsAuthCrud(t *testing.T) {
 	//		Secrets: []*Secret{
 	//			{
 	//				Name:      "baz",
-	//				Namespace: "redns",
+	//				Namespace: "testns2",
 	//				Generator: AlphaGenerator{
 	//					Type:   "alpha",
 	//					Length: 10,
 	//				},
 	//			},
 	//		},
-	//		Namespace: "redns",
+	//		Namespace: "testns2",
 	//	}, Prod)
 	//if err != nil {
 	//	log.Printf("error creating policy: %s", err)
@@ -65,14 +65,14 @@ func TestTlsAuthCrud(t *testing.T) {
 				Secrets: []*Secret{
 					{
 						Name:      "foo",
-						Namespace: "bluens",
+						Namespace: "testns1",
 						Generator: AlphaGenerator{
 							Type:   "alpha",
 							Length: 10,
 						},
 					},
 				},
-				Namespace: "bluens",
+				Namespace: "testns1",
 			},
 			map[string]interface{}{
 				"allowed_common_names":         []interface{}{"testbed01.inf.scribd.com"},
@@ -83,11 +83,11 @@ func TestTlsAuthCrud(t *testing.T) {
 				"allowed_uri_sans":             []interface{}{},
 				"bound_cidrs":                  []interface{}{"10.171.154.154"},
 				"certificate":                  "-----BEGIN CERTIFICATE-----\nMIIF/jCCA+agAwIBAgIJALblM1q8ZozAMA0GCSqGSIb3DQEBCwUAMIGZMQswCQYD\nVQQGEwJVUzELMAkGA1UECAwCQ0ExFjAUBgNVBAcMDVNhbiBGcmFuY2lzY28xFDAS\nBgNVBAoMC1NjcmliZCBJbmMuMREwDwYDVQQLDAhPcHMgVGVhbTEdMBsGA1UEAwwU\nU2NyaWJkIEluYy4gUm9vdCBDQSAxHTAbBgkqhkiG9w0BCQEWDm9wc0BzY3JpYmQu\nY29tMB4XDTE4MDYwNzIxNTkyOFoXDTI4MDYwNDIxNTkyOFowezELMAkGA1UEBhMC\nVVMxCzAJBgNVBAgMAkNBMRQwEgYDVQQKDAtTY3JpYmQgSW5jLjERMA8GA1UECwwI\nT3BzIFRlYW0xFzAVBgNVBAMMDlNjcmliZCBIb3N0IENBMR0wGwYJKoZIhvcNAQkB\nFg5vcHNAc2NyaWJkLmNvbTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIB\nAKwHZntYHGHLZ1Dzfd13GVJUfZryAicZg97Y5ALkqw87bLwBqY8K5kPmrpq4Vd2K\nwiizUQ0fHNCSZCuwDbUZQSiXIGCjFGISY0E+VVJo3as3fkcUaB6edUkBEzQDa3Jp\nIeRpM00x/jBpoKMAKGq3CZvQ3KIxZNvnFZr2t90ok+u988I89fi0wStco1A5UmE4\nlVyD7gkZGbMdLjUyIeDjtRIR8iGb6vDzljZ44CYd6LctEDZEmRAI7XDnt7/lzv29\nyaOoHaoZwgw7NzRLHC1EFJMnVT9dG9/pdO2Fgf3olAx0tZB0FSuBReTsDQmadSc1\nRXtwNeRJdjdHBTKEXrjepnYtk+fZnP2UEHUY/cHPwGk0slYHHA5pVIDkezD49D82\nO8hc6dLz6eUEBOtthZkeoFNe5+HStRs0ZYXLW/7Euiy9KBzk4NQ8RcJkcdafvCjF\nRvnJORnn2KR1ydVKXscCHplvJL3CR9erAOOQ2zlNxuL9ZAU/FBHy1eYwWz3OFJSQ\n1G5sZljE2G0nXYiPgKwUubR0JZnT3sh7SPps+xjMsOZpdhcgTSrlJuTPZURU2D6y\nyWuAiDONLavBayNel2Xe5U+Se8St4+86okh4E5kM2gnOfI6h2oRlZ+ClDwcnNyV2\nZzvWrPvTR+mPEN7KTh6kTzbDLEcWYmPUN5K+N9sHnKlRAgMBAAGjZjBkMB0GA1Ud\nDgQWBBSw8n2IHR4S2SmNm03l2x0ZyU8pkzAfBgNVHSMEGDAWgBRxanincb6eDh6h\nVLfN7zzjS/8GXTASBgNVHRMBAf8ECDAGAQH/AgEAMA4GA1UdDwEB/wQEAwIBhjAN\nBgkqhkiG9w0BAQsFAAOCAgEAJf3/rif8W3YQLOtR5MTcskxcv9WLj7EgDjT+AvQ3\n8POHE+fUz8u4TQ6811CqGHgfm12OXaoC/oWWY6T382WVguUxgBEOES4H5BnCY2ts\n+WY5OCE7vttVS3Q7A9W0XrMtnUN2KdhLplpPxcchzb8+Ulv4ysEvvzr9qy9UUpVd\nuLZAWL55WI1rF5rdPjkxDd/Zg3MTrqyNHvXYUjBFGfbU9+WJ0t1C8vWSIAX471ij\nOJyysNESz83ZBSZbifwEOmMU3IQHixjGhmS3gYX9qZrjd/i5R/8N5qcLdYDOFMoG\nn2M23HwADdZfVGMQX9nZEqV5y07Q2JoD1CRpbTePPB2RPSFbUGKMFQZj5KJQSW0r\n2shuAVUjjEbR6NA1iMrFBN8fxXHh7oMe+8aA51sX3RtDnzr9+TGpdEoDAdPdKVvu\nSka7woQCqBDfrZ4FFBFCApoicoZeozCeWcexFgO1USOYm/v1/gp2ikstHSSEm1j+\nPDpCMveI+puZxQhgUvc8OCHrfcBEHkUMEzZ18Q2w9ekTNbqBeQt3nuUdi2D7JOuI\nD3DrzKG87DxZjnjOqvhp2Alq84UParOejEk4+iS1hgLApVf8nMeThoXRKUEOF7KY\nHzMKYQXmTRJV3jB1uD/1ibA9MpMVEbNN3yjPvY6wmCE3ydOBC3/XQgcooez8af7w\n8no=\n-----END CERTIFICATE-----",
-				"display_name":                 "prod-bluens-app1",
+				"display_name":                 "prod-testns1-app1",
 				"max_ttl":                      json.Number("0"),
 				"period":                       json.Number("0"),
 				"policies": []interface{}{
-					"prod-bluens-app1",
+					"prod-testns1-app1",
 				},
 				"required_extensions": []interface{}{},
 				"ttl":                 json.Number("0"),
@@ -106,8 +106,8 @@ func TestTlsAuthCrud(t *testing.T) {
 				"max_ttl":                      json.Number("0"),
 				"period":                       json.Number("0"),
 				"policies": []interface{}{
-					"prod-bluens-app1",
-					"prod-bluens-app2",
+					"prod-testns1-app1",
+					"prod-testns1-app2",
 				},
 				"required_extensions": []interface{}{},
 				"ttl":                 json.Number("0"),
@@ -120,7 +120,7 @@ func TestTlsAuthCrud(t *testing.T) {
 		//		Secrets: []*Secret{
 		//			{
 		//				Name:      "foo",
-		//				Namespace: "redns",
+		//				Namespace: "testns2",
 		//				Generator: AlphaGenerator{
 		//					Type:   "alpha",
 		//					Length: 10,
@@ -128,13 +128,13 @@ func TestTlsAuthCrud(t *testing.T) {
 		//			},
 		//			{
 		//				Name:      "bar",
-		//				Namespace: "redns",
+		//				Namespace: "testns2",
 		//				Generator: UUIDGenerator{
 		//					Type: "uuid",
 		//				},
 		//			},
 		//		},
-		//		Namespace: "redns",
+		//		Namespace: "testns2",
 		//	},
 		//	map[string]interface{}{
 		//		"allowed_common_names":         []interface{}{"testbed01.inf.scribd.com"},
@@ -149,7 +149,7 @@ func TestTlsAuthCrud(t *testing.T) {
 		//		"max_ttl":                      json.Number("0"),
 		//		"period":                       json.Number("0"),
 		//		"policies": []interface{}{
-		//			"dev-redns-app2",
+		//			"dev-testns2-app2",
 		//		},
 		//		"required_extensions": []interface{}{},
 		//		"ttl":                 json.Number("0"),
@@ -168,8 +168,8 @@ func TestTlsAuthCrud(t *testing.T) {
 		//		"max_ttl":                      json.Number("0"),
 		//		"period":                       json.Number("0"),
 		//		"policies": []interface{}{
-		//			"dev-redns-app2",
-		//			"dev-redns-app3",
+		//			"dev-testns2-app2",
+		//			"dev-testns2-app3",
 		//		},
 		//		"required_extensions": []interface{}{},
 		//		"ttl":                 json.Number("0"),
