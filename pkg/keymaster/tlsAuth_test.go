0f18d730fb4be6e4180ffbccdc5fe0377f6427b4
Nik Ogura <nik@scribd.com>
2019-10-31 08:29:49 -0700

    removed debugging spew from tlsAuth_test

--- pkg/keymaster/tlsAuth_test.go
+++ pkg/keymaster/tlsAuth_test.go
@@ -3,7 +3,6 @@ package keymaster
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
 	"log"
 	"reflect"
@@ -204,11 +203,6 @@ func TestTlsAuthCrud(t *testing.T) {
 				fmt.Printf("Failed reading auth: %s", err)
 				t.Fail()
 			}
-
-			fmt.Printf("--- Expected ---\n")
-			spew.Dump(tc.first)
-			fmt.Printf("--- Actual ---\n")
-			spew.Dump(authData)
 
 			assert.True(t, reflect.DeepEqual(authData, tc.first))
 
