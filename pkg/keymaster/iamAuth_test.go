f3dacc97ee4be424624584d8d3d5f17f88dc36b3
Nik Ogura <nik@scribd.com>
2019-11-11 10:09:13 -0800

    added logging to iam auth tests

--- pkg/keymaster/iamAuth_test.go
+++ pkg/keymaster/iamAuth_test.go
@@ -150,6 +150,7 @@ func TestIamAuthCrud(t *testing.T) {
 
 	// don't run tests if we cannot get the aws region (means we're not running in aws)
 	if awsRegion != "" {
+		fmt.Printf("Running in AWS.  Testing IAM Auth")
 		for _, tc := range inputs {
 			t.Run(tc.name, func(t *testing.T) {
 				policy, err := km.NewPolicy(tc.role, DEV)
@@ -227,6 +228,8 @@ func TestIamAuthCrud(t *testing.T) {
 			})
 		}
 
+	} else {
+		fmt.Printf("Not running in AWS.  Skipping IAM Auth tests.")
 	}
 
 }
